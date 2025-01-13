f646249567077ecb6ed8b40e69ae61b6d64a74a5
tcypig <tianyuan1007@gamil.com>
2025-01-13 14:46:23 -0800

    add textInput

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
+import { useState } from 'react';
 
 
 export default function App() {
+  const [text, setText] = useState("");
+
   const appName = "My Awesome App";
+  function updateText(changedText: string) {
+    // update the text state
+    setText(changedText);
+  }
+
   return (
     <View style={styles.container}>
       {/* <Text>Welcome to {appName}</Text> */}
       <StatusBar style="auto" />
       <Header name={appName}/>
+      {}
+      <TextInput 
+        value={text} 
+        onChangeText={updateText}
+      />
     </View>
   );
 }
