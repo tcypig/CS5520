d28012f4970e7f660223e11d918b7e84bdf341ac
tcypig <tianyuan1007@gamil.com>
2025-02-24 20:56:43 -0800

    Refactor: Update Header component for dynamic padding and responsive font size based on window dimensions

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, Text, useWindowDimensions, View } from 'react-native'
-import React from 'react'
+import { Dimensions, StyleSheet, Text, useWindowDimensions, View } from 'react-native'
+import React, { useEffect } from 'react'
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 // type HeaderProps
 interface HeaderProps {
@@ -8,19 +10,24 @@ interface HeaderProps {
 
 export default function Header({name}: HeaderProps) {
   const {width, height} = useWindowDimensions();
+  const paddingVerticalDynamic = height < 415 ? 0 : 10;
 
   return (
     <View>
-      <Text style={[styles.header, {padding: height < 415 ? 0 : 10}]}>Welcome to {name}</Text>
+      <Text style={[styles.header, {padding: paddingVerticalDynamic}]}>
+        Welcome to {name}
+      </Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   header: {
-    fontSize: 26,
+    // fontSize: 26,
+    fontSize: windowWidth < 380 ? 20 : 26,
+    paddingHorizontal: windowWidth < 380 ? 10 : 20,
     color: '#9900cc',
     borderWidth: 2,
-    borderColor: '#9900cc',
+    borderColor: 'purple',
   }
 })
