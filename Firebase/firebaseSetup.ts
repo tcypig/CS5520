e1dae5a823dbf8aa3be7cc6d791a5305f314b8e9
tcypig <tianyuan1007@gamil.com>
2025-03-10 13:43:43 -0700

    Feat: Integrate Firebase Authentication with React Native Async Storage for persistence

--- Firebase/firebaseSetup.ts
+++ Firebase/firebaseSetup.ts
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
@@ -18,3 +21,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const database = getFirestore(app);
+// export const auth = getAuth(app)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
